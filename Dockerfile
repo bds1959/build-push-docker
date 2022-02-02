49523e6086c1d500c7135afede3860472f51eda9
bds1959 <murali@banyandata.com>
2022-02-02 10:44:28 +0530

    modified files

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,25 @@
-FROM alpine:latest
+#FROM alpine:latest
 
-RUN  apk add docker
+#RUN  apk add docker
 
-COPY docker-run.sh /docker-run.sh
+FROM python:3.8-slim-buster
 
-RUN chmod +x /docker-run.sh
+# Install the toolset.
+RUN apt -y update && apt -y install curl \
+    && pip install awscli \ 
+    && apt install docker.io -y \
+    && curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash \
+    && curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.19.0/bin/linux/amd64/kubectl \
+    && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl
 
-ENTRYPOINT ["/docker-run.sh"]
+RUN chmod 666 /var/run/docker.sock:/var/run/docker.sock
+    
+COPY deploy.sh /usr/local/bin/deploy
+
+CMD deploy
+
+#COPY docker-run.sh /docker-run.sh
+
+#RUN chmod +x /docker-run.sh
+
+#ENTRYPOINT ["/docker-run.sh"]
---
+++ deploy.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+echo $DOCKER_PASSWORD | docker login --username=$DOCKER_USER_NAME $CONTAINER_REGISTRY_URL --password-stdin
+image_name=$REPOSITORY_NAME:$IMAGE_VERSION
+image_url=$CONTAINER_REGISTRY_URL/$DOCKER_USER_NAME/$REPOSITORY_NAME:$IMAGE_VERSION
+docker_file=$DOCKER_FILE
+if [[ "${docker_file}" = "" ]]; then
+    docker_file="."
+fi
+docker build -t ${image_name} ${docker_file}
+docker tag ${image_name} ${image_url}
+docker push ${image_url}
+
+#output
+echo "::set-output name=image_url::${image_url}"
+
+# Login to Kubernetes Cluster.
+if [ -n "$CLUSTER_ROLE_ARN" ]; then
+    aws eks \
+        --region ${AWS_REGION} \
+        update-kubeconfig --name ${CLUSTER_NAME} \
+        --role-arn=${CLUSTER_ROLE_ARN}
+else
+    aws eks \
+        --region ${AWS_REGION} \
+        update-kubeconfig --name ${CLUSTER_NAME} 
+fi
+
+# Helm Deployment
+DEPS_UPDATE_COMMAND="helm dependency update ${DEPLOY_CHART_PATH}"
+UPGRADE_COMMAND="helm upgrade --install --timeout ${TIMEOUT}"
+
+if [ "$DEPLOY_ATOMIC" = "true" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} --atomic"
+fi
+
+if [ "$DEPLOY_FORCE" = "true" ]; then
+  UPGRADE_COMMAND="${UPGRADE_COMMAND} --force"
+fi
+
+for config_file in ${DEPLOY_CONFIG_FILES//,/ }
+do
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
+done
+if [ -n "$DEPLOY_NAMESPACE" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} -n ${DEPLOY_NAMESPACE}"
+fi
+if [ -n "$DEPLOY_VALUES" ]; then
+    UPGRADE_COMMAND="${UPGRADE_COMMAND} --set ${DEPLOY_VALUES}"
+fi
+UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH}"
+echo "Executing: ${DEPS_UPDATE_COMMAND}"
+${DEPS_UPDATE_COMMAND}
+echo "Executing: ${UPGRADE_COMMAND}"
+${UPGRADE_COMMAND}
